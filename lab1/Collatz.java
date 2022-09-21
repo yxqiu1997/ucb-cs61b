51c49ef92ee42692833ecfd0d1e381e1c65acfaa
Yuxuan Qiu <yxqiu1997@tju.edu.cn>
2022-09-21 22:46:45 +0800

    Update Collatz.java

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -9,13 +9,10 @@ public class Collatz {
      * @return next number of the sequence
      */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
+        if (n <= 1) {
+            throw new IllegalArgumentException();
         }
+        return n % 2 == 0 ? n / 2 : 3 * n + 1;
     }
 
     public static void main(String[] args) {
